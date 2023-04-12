a6217b052515e62b782b64cfa72548a02065fdc7
BienBoy <285915362@qq.com>
2023-04-12 17:47:26 +0800

    静态组件——首页

---
+++ src/views/home/Board.vue
@@ -0,0 +1,40 @@
+<template>
+    <el-card class="box-card" shadow="hover">
+        <template #header>
+            <div class="card-header">
+                <span>通知</span>
+                <el-link class="right">更多</el-link>
+            </div>
+        </template>
+        <div v-for="o in 8" :key="o" class="text item">
+            {{ '通知标题' + o }}
+        </div>
+    </el-card>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+.box-card {
+    width: 450px;
+}
+
+.text {
+    font-size: 14px;
+}
+
+.right {
+    float: right;
+}
+
+.item {
+    margin-bottom: 18px;
+}
+
+.el-row {
+    display: flex;
+    align-items: center;
+}
+</style>
---
+++ src/views/home/Home.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="main">
+        <roll-notification/>
+        <el-carousel type="card">
+            <el-carousel-item v-for="item in 6" :key="item">
+                <h3>{{ item }}</h3>
+            </el-carousel-item>
+        </el-carousel>
+        <el-row class="board">
+            <Board/>
+            <Board/>
+        </el-row>
+    </div>
+</template>
+
+<script setup>
+import Board from "@/views/home/Board.vue";
+import RollNotification from "@/views/home/RollNotification.vue";
+</script>
+
+<style scoped>
+.main {
+    width: 80%;
+    margin: 0 auto;
+}
+
+.board {
+    margin-top: 40px;
+    display: flex;
+    justify-content: space-around;
+}
+</style>
+
+<style>
+.el-carousel__item h3 {
+    color: #475669;
+    opacity: 0.75;
+    line-height: 200px;
+    margin: 0;
+    text-align: center;
+}
+
+.el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+    background-color: #d3dce6;
+}
+</style>
